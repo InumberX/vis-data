937298a3f2168c86366c7021d3c4aceecacf8ee2
InumberX <inumberx@gmaile.com>
2020-09-18 17:20:14 +0900

    Update nuxt.config.js

--- src/nuxt/visData/nuxt.config.js
+++ src/nuxt/visData/nuxt.config.js
@@ -1,27 +1,40 @@
+const now = new Date();
+let year = now.getFullYear();
+let month = now.getMonth() + 1;
+month = ("0" + month).slice(-2);
+let day = now.getDate();
+day = ("0" + day).slice(-2);
+let hour = now.getHours();
+hour = ("0" + hour).slice(-2);
+let minute = now.getMinutes();
+minute = ("0" + minute).slice(-2);
+let second = now.getSeconds();
+second = ("0" + second).slice(-2);
+const cashBuster = "?ver=" + year + month + day + hour + minute + second;
+
 export default {
+  env: {
+    cashBuster: cashBuster
+  },
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+  target: "static",
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'visData',
+    title: "visData",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -29,15 +42,15 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build"
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    "@nuxtjs/pwa"
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
@@ -45,5 +58,13 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    filenames: {
+      app: () => "js/[name].js" + cashBuster,
+      chunk: () => "js/[name].js" + cashBuster,
+      css: () => "css/[name].css" + cashBuster,
+      img: () => "img/[name].[ext]" + cashBuster,
+      font: () => "font/[name].[ext]" + cashBuster,
+      video: () => "video/[name].[ext]" + cashBuster
+    }
   }
-}
+};
