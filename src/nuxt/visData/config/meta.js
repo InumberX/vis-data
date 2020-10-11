d75955136f34c8cc7a6c32ac1c13f919d5f7f665
InumberX <inumberx@gmaile.com>
2020-10-11 12:31:44 +0900

    Update meta.js

--- src/nuxt/visData/config/meta.js
+++ src/nuxt/visData/config/meta.js
@@ -6,14 +6,14 @@ export default [
   },
   {
     id: 'population',
-    route: '/population',
+    route: '/population/',
     title: '人口増減率',
     titleEn: 'population',
     img: '/img/img_main_visual_01.jpg'
   },
   {
     id: 'births',
-    route: '/births',
+    route: '/births/',
     title: '出生数',
     titleEn: 'births',
     img: '/img/img_main_visual_02.jpg'
