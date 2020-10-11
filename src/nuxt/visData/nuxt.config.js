efae8d6f2dc8415357a6feee4f7b19f10eeb56b3
InumberX <inumberx@gmaile.com>
2020-10-11 15:35:42 +0900

    Update nuxt.config.js

--- src/nuxt/visData/nuxt.config.js
+++ src/nuxt/visData/nuxt.config.js
@@ -99,7 +99,7 @@ export default {
       {
         hid: 'twitter:card',
         name: 'twitter:card',
-        content: 'summary_large_image'
+        content: 'summary'
       },
       {
         hid: 'twitter:site',
