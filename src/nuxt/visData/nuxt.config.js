26f583c84c7f0c1595d84a509d15d5b980e27b03
InumberX <inumberx@gmaile.com>
2020-11-26 12:52:58 +0900

    Update nuxt.config.js

--- src/nuxt/visData/nuxt.config.js
+++ src/nuxt/visData/nuxt.config.js
@@ -1,12 +1,13 @@
 const now = new Date();
-const cashBuster =
-  '?ver=' +
+const nowDatetime =
   now.getFullYear() +
   ('0' + (now.getMonth() + 1)).slice(-2) +
   ('0' + now.getDate()).slice(-2) +
   ('0' + now.getHours()).slice(-2) +
   ('0' + now.getMinutes()).slice(-2) +
   ('0' + now.getSeconds()).slice(-2);
+
+const cashBuster = '?ver=' + nowDatetime;
 
 const title = 'vis:Data';
 const protocol = 'https://';
@@ -246,6 +247,7 @@ export default {
     ]
   },
   workbox: {
+    workboxVersion: nowDatetime,
     offline: false,
     skipWaiting: true,
     clientsClaim: true,
